2c91f43265dde2ea48817036f0ef8fe0d4079a09
Christian Hellum Bye <christian.hellum.bye@gmail.com>
2021-12-15 12:43:44 -0800

    fix typo

--- fdtd.py
+++ fdtd.py
@@ -106,7 +106,7 @@ class simulator:
         if len(np.nonzero(self.loss)[0])>0:
             plt.axvspan(xmin=np.nonzero(self.loss)[0][0], xmax=np.nonzero(self.loss)[0][-1], ymin=-Z0, ymax=Z0, alpha=.5)
         elif len(np.nonzero(self.epsr-1)[0])>0:
-            plt.axvspan(xmin=np.nonzero(self.epsr-1)[0][0], xmax=np.nonzero(self.epsr-1)[0][-1]m ymin=-Z0, ymax=Z0, alpha=.3)
+            plt.axvspan(xmin=np.nonzero(self.epsr-1)[0][0], xmax=np.nonzero(self.epsr-1)[0][-1], ymin=-Z0, ymax=Z0, alpha=.3)
         plt.ylim(-Z0, Z0)
         #plt.xlim()
         plt.legend(loc='upper left')
